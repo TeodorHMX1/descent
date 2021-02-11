25aad3a9bd8e57a457a2035b9c0fa954ef7fdf0c
Teodor Grigor <grigor.teodor@gmail.com>
2021-02-11 23:32:14 +0200

    battery added

---
+++ Assets/Scripts/Items/FlashPattern.cs
@@ -0,0 +1,33 @@
+﻿namespace Items
+{
+	
+	/// <summary>
+	///     <para> FlashPattern </para>
+	///     <author> @TeodorHMX1 </author>
+	/// </summary>
+	public class FlashPattern
+	{
+		private int _time;
+		private bool _isDark;
+
+		/// <summary>
+		///     <para> IsDark </para>
+		///     <author> @TeodorHMX1 </author>
+		/// </summary>
+		public bool IsDark
+		{
+			get => _isDark;
+			set => _isDark = value;
+		}
+
+		/// <summary>
+		///     <para> Time </para>
+		///     <author> @TeodorHMX1 </author>
+		/// </summary>
+		public int Time
+		{
+			get => _time;
+			set => _time = value;
+		}
+	}
+}
--- Assets/Scripts/Items/HelmetManage.cs
+++ Assets/Scripts/Items/HelmetManage.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using ZeoFlow.Pickup;
 using ZeoFlow.Pickup.Interfaces;
 
@@ -11,10 +12,49 @@ namespace Items
 	public class HelmetManage : MonoBehaviour, IOnAttached
 	{
 		public Light helmetLight;
-		
+		public int rechargeTime = 400;
+
 		private bool _isBoxColliderNotNull;
 		private BoxCollider _boxCollider;
-		
+		private int _timer;
+		private int _index;
+		private bool _outOfBattery;
+		private int _outTime;
+
+		// unity 1 = 1frame
+		private readonly FlashPattern[] _lightPattern =
+		{
+			// initial battery capacity
+			new FlashPattern {IsDark = false, Time = 300},
+
+			// light pattern (first light then dark and so on)
+			new FlashPattern {IsDark = false, Time = 200},
+			new FlashPattern {IsDark = true, Time = 10},
+			new FlashPattern {IsDark = false, Time = 180},
+			new FlashPattern {IsDark = true, Time = 10},
+			new FlashPattern {IsDark = false, Time = 160},
+			new FlashPattern {IsDark = true, Time = 10},
+			new FlashPattern {IsDark = false, Time = 140},
+			new FlashPattern {IsDark = true, Time = 10},
+			new FlashPattern {IsDark = false, Time = 120},
+			new FlashPattern {IsDark = true, Time = 10},
+			new FlashPattern {IsDark = false, Time = 100},
+			new FlashPattern {IsDark = true, Time = 20},
+			new FlashPattern {IsDark = false, Time = 80},
+			new FlashPattern {IsDark = true, Time = 35},
+			new FlashPattern {IsDark = false, Time = 60},
+
+			// end pattern
+			new FlashPattern {IsDark = true, Time = 0},
+		};
+
+		public HelmetManage()
+		{
+			_timer = 0;
+			_index = 0;
+			_outTime = 0;
+		}
+
 		/// <summary>
 		///     <para> Start </para>
 		///     <author> @TeodorHMX1 </author>
@@ -33,7 +73,54 @@ namespace Items
 		public void ONUpdate(PlayerAttachSub playerAttachMenu)
 		{
 			if (_isBoxColliderNotNull) _boxCollider.enabled = false;
-			if (Input.GetKeyDown(KeyCode.F)) helmetLight.enabled = !helmetLight.enabled;
+			if (Input.GetKeyDown(KeyCode.F))
+			{
+				if (!_outOfBattery)
+				{
+					_index = 0;
+					_timer = 0;
+					helmetLight.enabled = !helmetLight.enabled;
+				}
+			}
+
+			if (!_outOfBattery)
+			{
+				if (_index < _lightPattern.Length)
+				{
+					Flashlight();
+				}
+			}
+			else
+			{
+				_outTime--;
+				if (_outTime <= 0)
+				{
+					_outOfBattery = false;
+				}
+			}
+		}
+
+		/// <summary>
+		///     <para> Flashlight </para>
+		///     <author> @TeodorHMX1 </author>
+		/// </summary>
+		private void Flashlight()
+		{
+			_timer++;
+			if (_timer < _lightPattern[_index].Time) return;
+			_timer = 0;
+
+			_index++;
+			if (_index < _lightPattern.Length)
+			{
+				helmetLight.enabled = !_lightPattern[_index].IsDark;
+				return;
+			}
+
+			_index = 0;
+			_outTime = rechargeTime;
+			_outOfBattery = true;
+			helmetLight.enabled = false;
 		}
 	}
 }
