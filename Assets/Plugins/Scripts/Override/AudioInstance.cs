0ce14bc7f86023d7135c7db8e9499e402fee8fd2
Teodor G <teo@zeoflow.com>
2021-03-02 20:45:50 +0200

    minor improvements to design

--- Assets/Plugins/Scripts/Override/AudioInstance.cs
+++ Assets/Plugins/Scripts/Override/AudioInstance.cs
@@ -14,8 +14,9 @@ namespace Override
         private static GameObject _audioInstances;
         private static AudioSource _audioSource;
         private static GameObject _this;
-        private string _characters= "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        private readonly string _characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
         private bool _isResumed;
+        private float _soundVolume;
 
         private void Awake()
         {
@@ -23,6 +24,7 @@ namespace Override
             {
                 _mInstance = this;
                 _this = gameObject;
+                _soundVolume = GetSoundVolume();
                 if (_audioInstances != null) return;
                 _audioInstances = new GameObject {name = "Audio Instances"};
                 _audioInstances.transform.SetParent(gameObject.transform);
@@ -33,17 +35,43 @@ namespace Override
             }
         }
 
+        private void Update()
+        {
+            if (Math.Abs(_soundVolume - GetSoundVolume()) > 0)
+            {
+                _soundVolume = GetSoundVolume();
+                var audioSources = GetAudioSources();
+                foreach (var audioSource in audioSources)
+                {
+                    audioSource.volume = _soundVolume;
+                }
+            }
+
+            if (Time.timeScale == 0f)
+            {
+                var audioSources = GetComponentsInChildren<AudioSource>();
+                foreach (var audioSource in audioSources) audioSource.Pause();
+                _isResumed = false;
+            }
+            else if (!_isResumed)
+            {
+                var audioSources = GetComponentsInChildren<AudioSource>();
+                foreach (var audioSource in audioSources)
+                    if (!audioSource.isPlaying)
+                        audioSource.Play();
+                _isResumed = true;
+            }
+        }
+
         public static string ID()
         {
             var idGenerated = "";
-            for(var i=0; i<20; i++)
+            for (var i = 0; i < 20; i++)
             {
                 idGenerated += _mInstance._characters[Random.Range(0, _mInstance._characters.Length)];
-                if (i % 4 == 0 && i != 0 && i != 20)
-                {
-                    idGenerated += "_";
-                }
+                if (i % 4 == 0 && i != 0 && i != 20) idGenerated += "_";
             }
+
             return idGenerated;
         }
 
@@ -75,15 +103,12 @@ namespace Override
                     throw new ArgumentOutOfRangeException(nameof(volume), volume, null);
             }
         }
-        
+
         public static void StopSound(string name, bool destroy = false)
         {
             _audioSource = GetAudioSource(name);
             _audioSource.Stop();
-            if (destroy)
-            {
-                DestroySource(name);
-            }
+            if (destroy) DestroySource(name);
         }
 
         private static AudioSource GetAudioSource(string name)
@@ -117,30 +142,11 @@ namespace Override
             if (findGameObj != null) Destroy(findGameObj.gameObject);
         }
 
-        private void Update()
+        private static AudioSource[] GetAudioSources()
         {
-            if (Time.timeScale == 0f)
-            {
-                var audioSources = GetComponentsInChildren<AudioSource>();
-                foreach (var audioSource in audioSources)
-                {
-                    audioSource.Pause();
-                }
-                _isResumed = false;
-            }
-            else if (!_isResumed)
-            {
-                var audioSources = GetComponentsInChildren<AudioSource>();
-                foreach (var audioSource in audioSources)
-                {
-                    if (!audioSource.isPlaying)
-                    {
-                        audioSource.Play();
-                    }
-                }
-                _isResumed = true;
-            }
+            return _audioInstances == null
+                ? new AudioSource[] { }
+                : _audioInstances.transform.GetComponentsInChildren<AudioSource>();
         }
-
     }
 }
