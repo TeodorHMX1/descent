84d855384305e9983fc9de84796360d87ccb0489
Teodor G <teo@zeoflow.com>
2021-03-03 12:39:41 +0200

    minor bug fixes

---
+++ Assets/Plugins/Scripts/Override/VSync.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using ZeoFlow;
+using ZeoFlow.PlayerMovement;
+
+namespace Override
+{
+	/// <summary>
+	///     <para> VSync </para>
+	///     <author> @TeodorHMX1 </author>
+	/// </summary>
+	public class VSync : MonoBehaviour
+	{
+
+		/// <summary>
+		///     <para> Update </para>
+		///     <author> @TeodorHMX1 </author>
+		/// </summary>
+		private void Update()
+		{
+			// Sync framerate to monitors refresh rate
+			QualitySettings.vSyncCount = 1;
+		}
+	}
+}
